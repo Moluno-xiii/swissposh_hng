174bce2e5e7fc9d2e7a7e6320631754e344d3133
Moluno-xiii <molunoprogress@gmail.com>
2024-07-06 10:56:38 +0100

    initialised tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-primary flex items-center justify-center text-4xl">
+      Welcome to SwissPosh
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2400px",
+      },
+      fontFamily: {
+        sans: "satoshi, serif",
+      },
+      colors: {
+        primary: "#001F3F",
+      },
+    },
+  },
+  plugins: [],
+};
