e0ccebe936b3e5e0cef8d500b8b1e803cc4c9db8
Moluno-xiii <molunoprogress@gmail.com>
2024-07-06 15:11:47 +0100

    build

--- src/pages/WomenPage.jsx
+++ src/pages/WomenPage.jsx
@@ -1,13 +1,3 @@
-// function WomenPage() {
-//   return (
-//     <div className="text-primary text-blue-500 flex items-center justify-center text-4xl">
-//       Welcome to Women Page
-//     </div>
-//   );
-// }
-
-// export default WomenPage;
-
 export default function WomenPage() {
   return (
     <div className="">
