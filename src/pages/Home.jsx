9425cce4435a66e2c14195e9fb23a12b5511fdcf
Moluno-xiii <molunoprogress@gmail.com>
2024-07-12 18:47:28 +0100

    cors error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,30 @@ import Header from "../components/Header";
 
 
 import { Outlet } from "react-router-dom";
+const fetchData2 = async () => {
+  try {
+    const response = await fetch(
+      "/api/products?organization_id=13cad8063ba940efbccda69212e11d26&reverse_sort=false&page=1&size=10&Appid=3FMR5O3PRSXTMG8&Apikey=7c133f07b8864976a3095c480e82577a20240712120853424774",
+    );
+
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("application/json")) {
+      throw new TypeError("Received non-JSON response");
+    }
+
+    const data = await response.json();
+    console.log(response);
+    console.log("Fetched data:", data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 function Home() {
+
   return (
-    <div className="text-primary flex flex-col min-h-[100dvh] justify-center max-w-[100vw]">
+    <div className="text-primary flex flex-col min-h-[100dvh] justify-center max-w-[100vw]" onClick={fetchData2}>
       <Header />
       <div className="mt-[52px] md:mt-[70px]">
       <Outlet />
