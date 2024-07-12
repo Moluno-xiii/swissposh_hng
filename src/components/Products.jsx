f1f9cdc4acb893fab1370b662aef79a9875d6041
Moluno-xiii <molunoprogress@gmail.com>
2024-07-12 16:04:16 +0100

    added 'add to cart' button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,7 +6,7 @@ const Products = ({ product }) => {
   const { imageUrl, productName, largeImageUrl } = product;
   const navigate = useNavigate()
   return (
-    <div className="flex max-h-[285px] max-w-[161px] flex-col md:max-h-[502px] md:max-w-[232px]" onClick={() => navigate("/shopping-bag")}>
+    <div className="flex max-h-[285px] max-w-[161px] flex-col md:max-h-[502px] md:max-w-[232px]">
       <img
         src={imageUrl}
         alt="image of the product"
@@ -17,7 +17,7 @@ const Products = ({ product }) => {
         alt="image of the product"
         className="hidden md:inline-block"
       />
-      <div className="flex h-[68px] flex-row items-center justify-between  bg-white px-2">
+      <div className="flex h-[68px] flex-row items-center justify-between bg-white px-2">
         <div className="flex flex-col justify-center gap-y-1 md:gap-y-2">
           <p className="h-[10px] text-[7.65px] font-bold leading-[10.32px] md:text-xs">
             {productName}
@@ -27,9 +27,15 @@ const Products = ({ product }) => {
             alt="colour palette"
             className="h-[5.1px] w-[22.3px] md:h-2 md:w-[35px]"
           />
-          <span className="text-[6.37px] md:text-[10px] leading-[8.6px] text-primary">
+          <span className="text-[6.37px] leading-[8.6px] text-primary md:text-[10px]">
             $300
           </span>
+          <button
+            className="rounded-sm bg-primary bg-opacity-20 text-xs md:rounded-md md:text-sm"
+            onClick={() => navigate("/shopping-bag")}
+          >
+            add to cart
+          </button>
         </div>
         <p className="text-[6.37px] font-black md:text-[10px]">New Arrival</p>
       </div>
