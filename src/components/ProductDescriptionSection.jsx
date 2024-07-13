e7a65f1b06292959f9bb3f7ad1dbf86e1e29d85e
Moluno-xiii <molunoprogress@gmail.com>
2024-07-13 09:21:21 +0100

    fix : image source issue

--- src/components/ProductDescriptionSection.jsx
+++ src/components/ProductDescriptionSection.jsx
@@ -4,7 +4,7 @@ const ProductDescriptionSection = () => {
     <div className="flex-1 bg-[#f5f5f5] p-4 flex flex-col">
       <p className="flex flex-row justify-between text-sm font-bold">
         oversized denim shirt
-        <img src="/public/favourite.svg" alt="favourites icon" />
+        <img src="/favourite.svg" alt="favourites icon" />
       </p>
       <span>$300</span>
       <span>light beige</span>
