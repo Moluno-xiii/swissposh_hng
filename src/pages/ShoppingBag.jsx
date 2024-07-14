53e7c03c625aef4b5ca4b74e7776da43cf6f0221
Moluno-xiii <molunoprogress@gmail.com>
2024-07-14 21:02:36 +0100

    fix : /shopping-bag

--- src/pages/ShoppingBag.jsx
+++ src/pages/ShoppingBag.jsx
@@ -38,7 +38,7 @@ const ShoppingBag = () => {
               <CartItem item={item} key={index} />
             ))}
           </ul>
-         {cart.length >= 1 && <button
+         {newCart.length >= 1 && <button
             onClick={() => handleClearCart()}
             className="rounded-md bg-red-700 px-4 py-2 text-primary transition-all duration-300 hover:bg-opacity-80"
           >
