02df092dcdc9c944f0e8e156b8c82a0da47bb64d
Moluno-xiii <molunoprogress@gmail.com>
2024-07-14 06:52:53 +0100

    fix : fetch specific men product successful

--- src/pages/ProductDescriptionMen.jsx
+++ src/pages/ProductDescriptionMen.jsx
@@ -1,8 +1,31 @@
+import { Suspense, lazy } from "react";
+import Loader from "../components/Loader";
+import { useLoaderData } from "react-router-dom";
+import { addToCart, increment, decrement } from "../store/men/menSlice";
+import { useSelector } from "react-redux";
+
+const ProductDescriptionImagesSection = lazy(
+  () => import("../components/ProductDescriptionImagesSection"),
+);
+import ProductDescriptionSection from "../components/ProductDescriptionSectionWomen";
 
 const ProductDescriptionMen = () => {
+  const item = useLoaderData();
+  const cart = useSelector((state) => state.men.cart);
   return (
-    <div>ProductDescriptionMen</div>
-  )
-}
+    <div className="mx-auto my-5 flex max-w-[1346px] flex-col gap-x-4 md:flex-row">
+      <Suspense fallback={<Loader />}>
+        <ProductDescriptionImagesSection item={item} />
+      </Suspense>
+      <ProductDescriptionSection
+        item={item}
+        addToCart={addToCart}
+        increment={increment}
+        decrement={decrement}
+        cart={cart}
+      />
+    </div>
+  );
+};
 
-export default ProductDescriptionMen
+export default ProductDescriptionMen;
