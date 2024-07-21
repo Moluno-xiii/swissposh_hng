d9b6a39a120e8f494d12888ac1f09bb160187e00
Moluno-xiii <molunoprogress@gmail.com>
2024-07-21 12:50:27 +0100

    fix : api issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/swissposh_hng/"
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.timbu.cloud",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
