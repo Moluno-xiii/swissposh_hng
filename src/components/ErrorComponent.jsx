39dee1297ac2a046c1fea1740d6131ddcd55a3e1
Moluno-xiii <molunoprogress@gmail.com>
2024-07-21 13:03:14 +0100

    fix : product page rerender issue

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -3,9 +3,16 @@ import { Link } from "react-router-dom";
 
 const ErrorComponent = ({ message }) => {
   return (
-    <div className="flex flex-col gap-y-1 h-[100dvh] md:gap-y-3 justify-center items-center">
-      <p className="font-bold text-red-600 text-xl md:text-4xl uppercase">{message}</p>
-      <span className="flex h-[23.96px] md:text-base w-[133px] items-center justify-center bg-primary capitalize text-white max-md:text-[9.59px] max-md:leading-[14.98px] md:h-10 md:w-[222px]">
+    <div className="flex h-[100dvh] flex-col items-center justify-center gap-y-1 md:gap-y-3">
+      <p className="text-xl font-bold uppercase text-red-600 md:text-4xl">
+        {message}
+      </p>
+      {!message && (
+        <p className="text-xl font-bold uppercase text-red-600 md:text-4xl">
+          An error occurred
+        </p>
+      )}
+      <span className="flex h-[23.96px] w-[133px] items-center justify-center bg-primary capitalize text-white max-md:text-[9.59px] max-md:leading-[14.98px] md:h-10 md:w-[222px] md:text-base">
         <Link to="/">Return To HomePage</Link>
       </span>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,12 @@ import Header from "../components/Header";
 import MobileFooterNavbar from "../components/MobileFooterNavbar";
 import Spinner from "../components/Spinner";
 import { Outlet, useNavigation } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
 
 
 function Home() {
   const navigation = useNavigation();
-    useEffect(() => {
-      toast.info(
-        "Please be patient, loading content for the first time may take a while because of the reverse proxy server to bypass CORS policies",
-      );
-    }, []);
   if (navigation.state === "loading") return <Spinner />
   return (
     <div className="flex min-h-[100dvh] max-w-[100vw] flex-col justify-center text-primary">
